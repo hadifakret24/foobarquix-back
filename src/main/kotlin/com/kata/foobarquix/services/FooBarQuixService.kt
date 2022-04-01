f14ea8600b0916d6aeb67fb1f3a5514b6b09717c
HadiPro <hadi.fakret@gmail.com>
2022-04-01 17:44:40 +0200

    Initial commit

---
+++ src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -0,0 +1,46 @@
+package com.kata.foobarquix.services
+
+import org.springframework.stereotype.Component
+
+@Component
+class FooBarQuixService {
+
+    fun convertNumber(inputNumber: Int): String {
+
+        var result: String = "";
+
+        if (inputNumber % 3 == 0) {
+            result += "Foo";
+        }
+        if (inputNumber % 5 == 0) {
+            result += "Bar";
+        }
+        if (inputNumber % 7 == 0) {
+            result += "Quix"
+        }
+
+        val inputNumberStr = inputNumber.toString();
+        val inputNumberCharArray: CharArray = inputNumberStr.toCharArray();
+        for(i in inputNumberCharArray) {
+            if (i == '3') {
+                result += "Foo";
+            }
+            if (i == '5') {
+                result += "Bar";
+            }
+            if (i == '7') {
+                result += "Quix";
+            }
+            if (i == '0') {
+                result += "Buzz";
+            }
+        }
+
+        if (result.isEmpty()) {
+            return inputNumberStr;
+        } else {
+            return result;
+        }
+    }
+
+}
---
+++ src/test/kotlin/com/kata/foobarquix/services/FooBarQuixApplicationTests.kt
@@ -0,0 +1,30 @@
+package com.kata.foobarquix.services
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class FooBarQuixServiceTest {
+	@Autowired
+	lateinit var fooBarQuixService: FooBarQuixService;
+
+	@Test
+	fun convertNumber() {
+		assertEquals("FooBarQuixBuzz", fooBarQuixService.convertNumber(0));
+		assertEquals("1", fooBarQuixService.convertNumber(1));
+		assertEquals("FooFoo", fooBarQuixService.convertNumber(3));
+		assertEquals("BarBar", fooBarQuixService.convertNumber(5));
+		//assertEquals("Quix", fooBarQuixService.convertNumber(7));
+		assertEquals("QuixQuix", fooBarQuixService.convertNumber(7));
+		assertEquals("Foo", fooBarQuixService.convertNumber(9));
+		assertEquals("BarBuzz", fooBarQuixService.convertNumber(10));
+		assertEquals("Quix", fooBarQuixService.convertNumber(49));
+		assertEquals("FooBar", fooBarQuixService.convertNumber(51));
+		assertEquals("BarFoo", fooBarQuixService.convertNumber(53));
+		assertEquals("FooFooFoo", fooBarQuixService.convertNumber(33));
+		assertEquals("FooQuix", fooBarQuixService.convertNumber(27));
+		assertEquals("FooBarBar", fooBarQuixService.convertNumber(15));
+	}
+}
